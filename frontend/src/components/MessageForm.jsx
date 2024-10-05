4a3c4e7f0f69d63a9089b71d8ad6551d8d86403f
Ganga Kailas <gangakailasnath@gmail.com>
2024-10-06 00:02:51 +0200

    message from homepage done

--- frontend/src/components/MessageForm.jsx
+++ frontend/src/components/MessageForm.jsx
@@ -1,11 +1,54 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const MessageForm = () => {
-  return (
-    <div>
-        
-    </div>
-  )
+    const [firstName, setFirstName] = useState("")
+    const [lastName, setLastName] = useState("")
+    const [email, setEmail] = useState("")
+    const [phone, setPhone] = useState("")
+    const [message, setMessage] = useState("")
+    const handleMessage = (e) => {
+        e.preventDefault()
+    }
+    return (
+        <div className='container form-component message-form'>
+            <h2>Send Us A Message</h2>
+            <form onSubmit={handleMessage}>
+                <div>
+                    <input 
+                        type="text" 
+                        placeholder="First Name" 
+                        value={firstName} 
+                        onChange={(e)=> setFirstName(e.target.value)}
+                    />
+                    <input 
+                        type="text" 
+                        placeholder="Last Name" 
+                        value={lastName} 
+                        onChange={(e)=> setLastName(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <input 
+                        type="text" 
+                        placeholder="Email" 
+                        value={email} 
+                        onChange={(e)=> setEmail(e.target.value)}
+                    />
+                    <input 
+                        type="text" 
+                        placeholder="Phone Number" 
+                        value={phone} 
+                        onChange={(e)=> setPhone(e.target.value)}
+                    /> 
+                </div>
+                <textarea rows={7} placeholder="Message" value={message} onChange={(e)=>setMessage(e.target.value)}
+                ></textarea>
+                <div style={{justifyContent: "center", alignItems: "center"}}>
+                    <button type= "submit ">Send</button>
+                </div>
+            </form>
+        </div>
+    )
 }
 
 export default MessageForm
