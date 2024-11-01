83f22f9eca186a936af6a46e3ca81f877fe269ba
Ganga Kailas <gangakailasnath@gmail.com>
2024-11-01 02:16:20 +0100

    frontend almost finished

--- dashboard/src/main.jsx
+++ dashboard/src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode, useState, createContext } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
+
+export const Context = createContext({ isAuthenticated: false });
+
+const AppWrapper = () => {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [user, setUser] = useState(false);
+
+  return (
+    <Context.Provider value={{ isAuthenticated, setIsAuthenticated, user, setUser }}>
+      <App />
+    </Context.Provider>
+  );
+};
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AppWrapper />
   </StrictMode>,
-)
+);
